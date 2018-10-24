7e9a391374f1e230931c519c7f410e4cef426f3f
Alex Dolzhenko <sitekarandash@gmail.com>
2018-10-24 17:24:48 +0300

    Add other elements

--- js/main.js
+++ js/main.js
@@ -33,5 +33,33 @@ $(document).ready(function () {
     $(this).parent().parent().toggleClass('open');
   });
 
+  // Custom Select
+
+  $('.selectpicker').selectpicker();
+
+  // Rating
+
+  var $star_rating = $('.rating-item');
+
+  var SetRatingStar = function() {
+    return $star_rating.each(function() {
+      if (parseInt($star_rating.siblings('input.rating-value').val()) >= parseInt($(this).data('rating'))) {
+        return $(this).addClass('active');
+      } else {
+        return $(this).removeClass('active');
+      }
+    });
+  };
+
+  $star_rating.on('click', function() {
+    $star_rating.siblings('input.rating-value').val($(this).data('rating'));
+    return SetRatingStar();
+  });
+
+  SetRatingStar();
+  $(document).ready(function() {
+
+  });
+
 
 });
